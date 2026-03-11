9624ec70a822d1d8dc0f4764231b9199f1e21e17
Gowtham Manoharan <gow99tham991@gmail.com>
2026-03-11 20:45:12 +0530

    Ignore TypeScript build errors

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    typescript: {
-        // Ignore TypeScript build errors
-        ignoreBuildErrors: true
-    }
+    env: {
+        stackbitPreview: process.env.STACKBIT_PREVIEW
+    },
+    trailingSlash: true,
+    reactStrictMode: true,
+    allowedDevOrigins: ['192.168.1.84']
 };
 
 module.exports = nextConfig;
